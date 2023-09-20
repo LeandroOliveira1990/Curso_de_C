2d00fd9af2a7fd7618cb11e6b973ffc9be98cf8f
Leandro de Oliveira Silva <110729164+LeandroOliveira1990@users.noreply.github.com>
2023-09-20 15:48:44 -0300

    Aula usando o define para criar constantes

---
+++ Curso_de_C/criar_constantes/criar_constantes.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define TAM 10
+
+int main(int argc, char const *argv[])
+{
+    //Imprime na tela
+    printf("%d\n\n", TAM);
+
+    //Contagem até 10
+    for(int i = 1; i <= TAM; i++){
+        printf("\n%d", i);
+
+    }
+    return 0;
+}
